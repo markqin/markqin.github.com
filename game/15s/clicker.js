456274e14ee74940b45c69e8cb489a9e0f220102
markqin <qclaaa@gmail.com>
2014-05-26 20:01:54 +0800

    fix

--- game/15s/clicker.js
+++ game/15s/clicker.js
@@ -110,4 +110,63 @@ restartBtn.addEventListener('click',function(ev){
 
 
 
+var shareInfo = {
+	appid: '',
+    imgUrl: '',
+    lineLink: '',
+	descContent: '',
+	shareTitle: '决战15秒，看谁的手指更快！'
+}
+
+
+
+function shareFriend() {
+    WeixinJSBridge.invoke('sendAppMessage',{
+        "appid": shareInfo.appid,
+        "img_url": shareInfo.imgUrl,
+        "img_width": "200",
+        "img_height": "200",
+        "link": shareInfo.lineLink,
+        "desc": shareInfo.descContent,
+        "title": shareInfo.shareTitle
+    }, function(res) {
+        //_report('send_msg', res.err_msg);
+    })
+}
+function shareTimeline() {
+    WeixinJSBridge.invoke('shareTimeline',{
+        "img_url": shareInfo.imgUrl,
+        "img_width": "200",
+        "img_height": "200",
+        "link": shareInfo.lineLink,
+        "desc": shareInfo.descContent,
+        "title": shareInfo.shareTitle
+    }, function(res) {
+           //_report('timeline', res.err_msg);
+    });
+}
+function shareWeibo() {
+    WeixinJSBridge.invoke('shareWeibo',{
+        "content": shareInfo.descContent,
+        "url": shareInfo.lineLink,
+    }, function(res) {
+        //_report('weibo', res.err_msg);
+    });
+}
+// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+    // 发送给好友
+    WeixinJSBridge.on('menu:share:appmessage', function(argv){
+        shareFriend();
+    });
+    // 分享到朋友圈
+    WeixinJSBridge.on('menu:share:timeline', function(argv){
+        shareTimeline();
+    });
+    // 分享到微博
+    WeixinJSBridge.on('menu:share:weibo', function(argv){
+        shareWeibo();
+    });
+}, false);
+
 
